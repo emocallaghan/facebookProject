e8b9a6fe8cdb95add710e65baaf17108a5419407
elizabeth ocallaghan <eocallaghan@eocallaghan-Latitude-E6430.(none)>
2014-02-26 17:53:17 -0500

    get and print all of a friend's news filtering out photos, and tags

--- FacebookCluster.py
+++ FacebookCluster.py
@@ -10,26 +10,31 @@ Created on Sun Feb 23 17:46:52 2014
 from pattern.web import *
 f = Facebook(license='CAAEuAis8fUgBAAG8SBjH7bEAimzm6shZBMmOFaginXZCuxdPftGBZBFfHnOYEZAWWBowqHCDzCKNniyeZAm6Feimonnjcgv3hFEG8CLKcbIPHn727Hf9wqPsPV9HNd0V7LHBiN3lUyxWawxmhKbZCtXDjVU6KZBGQ9rLqTgjiHnNvb3CZCkhVzpA36whZB7nRq9EZD')
 me = f.profile()
-"""
+
 my_friends = f.search(me[0], type=FRIENDS, count=400)
 for friend in my_friends:
-    friend_news = f.search(friend.id, type=NEWS, count=400)
-    for news in friend_news:
-        print news.text
+    friend_name = str(friend.text)
+    for news in f.search(friend.id, type=NEWS, count =100):
+        friend_text = str(news.text)
+        if (not friend_text.__contains__(friend_name)):
+            news_author = str(news.author)
+            if(news_author.__contains__(friend_name)):
+                print news.text
 
-"""  
+"""
+#print str(me[0])  
 for news in f.search(me[0], type=NEWS, count=100):
-    text = str(news.text)
-    #print text.contains(me(2))
+    #text = str(news.text)
+    #print text
+    #print str(post.id)
+    #print type(me)    
+    #print type(post)
+    author = str(news.author)
     name = str(me[1])
-    if(not text.__contains__(name)):
-        myPost = True
-        my_friends = f.search(me[0], type=FRIENDS, count =400)
-        for friend in my_friends:
-            friend_news = f.search(friend.id, type=NEWS, count =400)
-            if (friend_news.id == news.id):
-                myPost = False
-                break
-        if(myPost):
-            print repr(text)
+    if(author.__contains__(name)):
+        if()
+        print news.text
+        #print post.story_tags
+        
     
+    """
