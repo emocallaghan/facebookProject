2c502856e053b4065849c4b2544b059f6f8f9da7
elizabeth ocallaghan <eocallaghan@eocallaghan-Latitude-E6430.(none)>
2014-03-03 11:08:35 -0500

    commiting with positive and subjective poeople no doc strings

--- FacebookCluster.py
+++ FacebookCluster.py
@@ -4,9 +4,6 @@ Created on Sun Feb 23 17:46:52 2014
 
 @author: eocallaghan
 """
-import re, math
-from collections import Counter
-
 
 from pattern.en import *
 from pattern.web import *
@@ -18,7 +15,7 @@ f_shrin = Facebook(license='CAAEuAis8fUgBAGjbDK3oFYhbFCtymaT4emLFUO2gzgarzB5I9aG
 def get_posts(f): #Gets posts and saves in a dictionary
     posts_list = {}
     me = f.profile()
-    my_friends = f.search(me[0], type=FRIENDS, count = 10)
+    my_friends = f.search(me[0], type=FRIENDS, count = 200)
     for friend in my_friends:
         friend_name = str(friend.text)
         friend_news = f.search(friend.id, type=NEWS, count =10)
@@ -29,19 +26,23 @@ def get_posts(f): #Gets posts and saves in a dictionary
     return posts_list
 
 def find_sentiments(friends):
-    print friends
     friend_sentiment = {}
     for key in friends:
-        print key
-        print friends[key]
         analyzed_sentiment = sentiment(friends[key])
-        print analyzed_sentiment[0]
         friend_sentiment[key] = analyzed_sentiment[0]
         
     return friend_sentiment
 
-def sort_sentiments(sentiments):
-    mostPositive = 0;
+def find_subjectivity(friends):
+    friend_subjective = {}
+    for key in friends:
+        analyzed_subjective = sentiment(friends[key])
+        friend_subjective[key] = analyzed_subjective[1]
+        
+    return friend_subjective
+    
+def most_Positive_Person(sentiments):
+    mostPositive = -1;
     mostPositivePerson = ""
     for key in sentiments:
         if sentiments[key]>mostPositive:
@@ -49,75 +50,67 @@ def sort_sentiments(sentiments):
             mostPositivePerson = key
     return mostPositivePerson
     
-def sort_sentiments_all(sentiments):
-    return sorted(sentiments)
+def most_negative_Person(sentiments):
+    mostNegative = 1;
+    mostNegativePerson = ""
+    for key in sentiments:
+        if sentiments[key]<mostNegative:
+            mostNegative = sentiments[key]
+            mostNegativePerson = key
+    return mostNegativePerson
     
-#sentimentList = find_sentiments(get_posts(f_lizzie))
+def sort_sentiments_postive(sentiments):
+    positivePeople = []
+    for people in sorted(sentiments, key=sentiments.get, reverse=True):
+        positivePeople.append(people)
+    return positivePeople
+        
+def sort_sentiments_negative(sentiments):
+    negativePeople = []
+    for people in sorted(sentiments, key=sentiments.get):
+        negativePeople.append(people)
+    return negativePeople
+    
+def most_subjective(subjectivity):
+    mostSubjective = 0;
+    mostSubjectivePerson = ""
+    for key in subjectivity:
+        if subjectivity[key]>mostSubjective:
+            mostSubjective = subjectivity[key]
+            mostSubjectivePerson = key
+    return mostSubjectivePerson
+    
+def most_objective(subjectivity):
+    mostObjective = 1;
+    mostObjectivePerson = ""
+    for key in subjectivity:
+        if subjectivity[key]<mostObjective:
+            mostObjective = subjectivity[key]
+            mostObjectivePerson = key
+    return mostObjectivePerson
+    
+def sort_subjectivity(subjectivy):
+    subjectivePeople = []
+    for people in sorted(subjectivy, key=subjectivy.get, reverse=True):
+        subjectivePeople.append(people)
+    return subjectivePeople
+        
+def sort_objectivity(subjectivy):
+    objectivePeople = []
+    for people in sorted(subjectivy, key=subjectivy.get):
+        objectivePeople.append(people)
+    return objectivePeople
+    
+posts = get_posts(f_lizzie)
+    
+sentimentList = find_sentiments(posts)
+subjectiveList = find_subjectivity(posts)
 
-#print sort_sentiments(sentimentList)
+print sort_sentiments_postive(sentimentList)
+print sort_sentiments_negative(sentimentList)
 
+print sort_subjectivity(subjectiveList)
+print sort_objectivity(subjectiveList)
 
-
-
-
-def word_frequencies(newsFeed):
-    word_count = {}
-    for i in range(len(newsFeed)):
-        current_string = newsFeed[i]
-        for j in range(len(current_string)):
-            current_word = current_string(j)
-            if(word_count.contains_key(current_word)):
-                word_count[current_word] = word_count[current_word]+1
-            else:
-                word_count[current_word] = 1
-    return word_count
-
-
-
-
-
-
-
-
-
-
-WORD = re.compile(r'\w+')
-#http://stackoverflow.com/questions/15173225/how-to-calculate-cosine-similarity-given-2-sentence-strings-python
-def get_cosine(vec1, vec2):
-     intersection = set(vec1.keys()) & set(vec2.keys())
-     numerator = sum([vec1[x] * vec2[x] for x in intersection])
-
-     sum1 = sum([vec1[x]**2 for x in vec1.keys()])
-     sum2 = sum([vec2[x]**2 for x in vec2.keys()])
-     denominator = math.sqrt(sum1) * math.sqrt(sum2)
-
-     if not denominator:
-        return 0.0
-     else:
-        return float(numerator) / denominator
-
- 
-def text_to_vector(text):
-     words = WORD.findall(text)
-     return Counter(words)
-     
-def dictionary_to_vectors(dictionary):
-    for key in dictionary:
-        dictionary[key] = text_to_vecotr(dictionary[key])
-    return dictionary
-
-def get_all_cosines(dictionary):
-    workingDictionary = dictionary_to_vectors(dictionary)
-    for key in dictionary:
-        for secondKey in dictionary:
-            
-text1 = 'This is a foo bar sentence .'
-text2 = 'This sentence is similar to a foo bar sentence .'
-
-vector1 = text_to_vector(text1)
-vector2 = text_to_vector(text2)
-
-cosine = get_cosine(vector1, vector2)
-
-
-print 'Cosine:', cosine
+print most_subjective(subjectiveList)
+print most_objective(subjectiveList)
